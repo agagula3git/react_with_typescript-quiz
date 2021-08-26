ee1b1cab393d48a0acced34371273d65b15531e1
Ajdin Gagula <agagula3@etf.unsa.ba>
2021-08-26 16:05:27 +0200

    finished

---
+++ src/API.tsx
@@ -0,0 +1,34 @@
+import { shuffleArray } from "./utils";
+
+// type checking by using interface
+interface Question{
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+// create a new type/object by adding a key/value pair to interface Question 
+export type QuestionsState = Question & { answers: string[]};
+
+// using enum to represent a fixed number of possible values
+export enum Difficulty{
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    
+        const data = await (await fetch(endpoint)).json();
+        return data.results.map((question: Question) => (
+            {
+                ...question,
+                answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+            }
+        ))
+    
+}
---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+//Components
+import QuestionCard from './components/QuestionCard';
+//Types
+import { QuestionsState, Difficulty } from './API';
+//Styles
+import { GlobalStyle, Wrapper} from './App.styles'
+const TOTAL_QUESTIONS = 10;
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ))
+
+  const startTrivia = async () =>{
+    setLoading(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) =>{
+    if(!gameOver){
+      const answer = e.currentTarget.value;
+      //check answer
+      const correct = questions[number].correct_answer === answer;
+      //if answer is correct then increase score for one
+      if(correct){
+        setScore(prev => prev + 1);
+      }
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+      setUserAnswers(prev => [...prev, answerObject])
+    }
+  }
+
+  const nextQuestion = () => {
+    // move on to the next question if not the last question
+    const next = number + 1;
+    next === TOTAL_QUESTIONS ? setGameOver(true) : setNumber(next);
+  }
+
+  return (
+    <>
+    <GlobalStyle/>
+    <Wrapper>
+      <h1>REACT QUIZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
+      {loading ? <p>Loading Questions...</p> : null }
+      {!loading && !gameOver && (
+        <QuestionCard 
+          questionNr={number + 1}
+          totalQuestions = {TOTAL_QUESTIONS}
+          question = {questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number != TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+    </Wrapper>
+    </>
+  );
+}
+
+export default App;
